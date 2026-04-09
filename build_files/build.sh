4a267ac91731446dac2bba5f2d7ff895e9315035
roma <roma@sixtyten>
2026-04-08 23:09:57 -0700

    resetting project back to basics

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,11 +2,11 @@
 set -ouex pipefail
 
 # cleanup old repo dust
-rm -f /etc/yum.repos.d/_copr_ublues-os-akmods.repo
-rm -f /etc/yum.repos.d/_copr_avengemedia-dms.repo
+#rm -f /etc/yum.repos.d/_copr_ublues-os-akmods.repo
+#rm -f /etc/yum.repos.d/_copr_avengemedia-dms.repo
 
 dnf5 -y copr enable ublue-os/akmods
-dnf5 -y copr enable avengemedia/dms
+#dnf5 -y copr enable avengemedia/dms
 dnf -y copr enable lihaohong/yazi
 
 ### Install packages
@@ -16,37 +16,21 @@ dnf -y copr enable lihaohong/yazi
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 dnf5 install -y \
-	niri \
-	greetd \
-	dbus-daemon \
-	tuigreet \
-	kitty \
-	fish \
-	waybar \
-	mako \
-	fuzzel \
-	xwayland-satellite \
-	swaybg \
-	wl-clipboard \
-	yazi \
-	mc \
-	dms \
-	dms-greeter \
-	cliphist
+	yazi
 
-install -Dm644 /ctx/files/etc/greetd/config.toml /etc/greetd/config.toml
-install -Dm644 /ctx/files/etc/niri/config.kdl /etc/niri/config.kdl
-install -Dm644 /ctx/files/etc/skel/.config/niri/config.kdl /etc/skel/.config/niri/config.kdl
-install -Dm644 /ctx/files/etc/skel/.config/waybar/config.jsonc /etc/skel/.config/waybar/config.jsonc
-install -Dm644 /ctx/files/etc/skel/.config/waybar/style.css /etc/skel/.config/waybar/style.css
+#install -Dm644 /ctx/files/etc/greetd/config.toml /etc/greetd/config.toml
+#install -Dm644 /ctx/files/etc/niri/config.kdl /etc/niri/config.kdl
+#install -Dm644 /ctx/files/etc/skel/.config/niri/config.kdl /etc/skel/.config/niri/config.kdl
+#install -Dm644 /ctx/files/etc/skel/.config/waybar/config.jsonc /etc/skel/.config/waybar/config.jsonc
+#install -Dm644 /ctx/files/etc/skel/.config/waybar/style.css /etc/skel/.config/waybar/style.css
 
 dnf5 -y copr disable ublue-os/akmods
-dnf5 -y copr disable avengemedia/dms
+#dnf5 -y copr disable avengemedia/dms
 dnf  -y copr disable lihaohong/yazi
 
 systemctl enable podman.socket
-systemctl enable greetd.service
-systemctl set-default graphical.target
+#systemctl enable greetd.service
+#systemctl set-default graphical.target
 
 # cleanup
 rm -rf /run/dnf /run/selinux-policy /tmp/*
